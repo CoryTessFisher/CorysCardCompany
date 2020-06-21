bf84866c94410d09815fcd15fa49dc184222e007
CoryTessFisher <66070971+CoryTessFisher@users.noreply.github.com>
2020-06-21 13:29:55 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Cory's Card Company</title>
+</head>
+
+<body>
+
+
+
+<div align="center">
+  <p><img src="images/CoryFisher.gif" alt="Cory Fisher" width="256" height="320" border="3" /></p>
+  <p>Available Files</p>
+  <p><BR />
+    
+    <?php
+// open this directory 
+$myDirectory = opendir(".");
+
+// get each entry
+while($entryName = readdir($myDirectory)) {
+	$dirArray[] = $entryName;
+}
+
+// close directory
+closedir($myDirectory);
+
+//	count elements in array
+$indexCount	= count($dirArray);
+$nonhiddenindexCount = $indexCount - 2;
+Print ("$nonhiddenindexCount files<br>\n");
+
+// sort 'em
+sort($dirArray);
+
+// print 'em
+print("<TABLE border=1 cellpadding=5 cellspacing=0 class=whitelinks>\n");
+print("<TR><TH>Filename</TH><th>Filetype</th><th>Filesize</th></TR>\n");
+// loop through the array of files and print them all
+for($index=0; $index < $indexCount; $index++) {
+        if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
+		print("<TR><TD><a href=\"$dirArray[$index]\">$dirArray[$index]</a></td>");
+		print("<td>");
+		print(filetype($dirArray[$index]));
+		print("</td>");
+		print("<td>");
+		print(filesize($dirArray[$index]));
+		print("</td>");
+		print("</TR>\n");
+	}
+}
+print("</TABLE>\n");
+?>
+      </p>
+</div>
+</body>
+</html>
